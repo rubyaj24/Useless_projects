8d9c1e1e077c3d1e7e42ce1c6ef2cf72f9c3e0ad
rubyaj24 <amaljithmvinod@gmail.com>
2024-11-02 15:35:35 +0530

    LAST CORRECTIONS

--- WindowSimulator.java
+++ WindowSimulator.java
@@ -1,6 +1,8 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.AbstractBorder;
+import java.awt.geom.RoundRectangle2D;
 import java.util.Random;
 
 public class WindowSimulator extends JFrame {
@@ -11,28 +13,54 @@ public class WindowSimulator extends JFrame {
         setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 
         // Set the content pane color to translucent
-        getContentPane().setBackground(new Color(0, 0, 0, 128)); // RGBA, where A is the alpha for transparency
+        getContentPane().setBackground(new Color(0, 0, 0, 128));
         setLayout(new BorderLayout());
 
-        // Add a JLabel with the text "this is useless" and "you're trapped" on a new line
-        JLabel label = new JLabel("<html><div style='text-align: center;'>this is useless<br>you're trapped</div></html>", SwingConstants.CENTER);
-        label.setFont(new Font("Arial", Font.PLAIN, 14));
-        label.setForeground(Color.WHITE);
-        add(label, BorderLayout.CENTER);
+        // Add a navigation bar at the top
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("Nothing");
+        JMenu editMenu = new JMenu("Absolutely nothing");
+        JMenu helpMenu = new JMenu("Definitely nothing");
+        JMenu uselessProjectsMenu = new JMenu("USELESS PROJECTS");
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+        menuBar.add(helpMenu);
+        menuBar.add(Box.createHorizontalGlue()); // Push the "USELESS PROJECTS" menu to the right
+        menuBar.add(uselessProjectsMenu);
+        setJMenuBar(menuBar);
 
-        // Add a JLabel with the text "USELESS PROJECTS" using * symbol as the title
-        JLabel titleLabel = new JLabel("* USELESS PROJECTS *", SwingConstants.CENTER);
-        titleLabel.setFont(new Font("Arial", Font.BOLD, 16));
-        titleLabel.setForeground(Color.YELLOW);
-        add(titleLabel, BorderLayout.NORTH);
+        // Add a JLabel with the text "this is useless" and "you're trapped"
+        JLabel label = new JLabel("<html><div style='text-align: center;'>this is useless<br>you're trapped</div></html>", SwingConstants.CENTER);
+        label.setFont(new Font("Monospaced", Font.ITALIC, 14));
+        label.setForeground(Color.BLACK);
+
+        // Create a JPanel with a rounded border
+        JPanel roundedPanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2 = (Graphics2D) g;
+                g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                g2.setColor(getBackground());
+                g2.fill(new RoundRectangle2D.Double(0, 0, getWidth(), getHeight(), 30, 30));
+            }
+        };
+        roundedPanel.setOpaque(false);
+        roundedPanel.setLayout(new BorderLayout());
+        roundedPanel.add(label, BorderLayout.CENTER);
+        roundedPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
         // Add padding around the content
         JPanel paddingPanel = new JPanel();
         paddingPanel.setOpaque(false);
         paddingPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
         paddingPanel.setLayout(new BorderLayout());
-        paddingPanel.add(label, BorderLayout.CENTER);
+        paddingPanel.add(roundedPanel, BorderLayout.CENTER);
         add(paddingPanel, BorderLayout.CENTER);
+
+        // Add an ImageIcon
+        ImageIcon icon = new ImageIcon("logo.png");
+        this.setIconImage(icon.getImage());
 
         addWindowListener(new WindowAdapter() {
             @Override
@@ -65,6 +93,7 @@ public class WindowSimulator extends JFrame {
         setVisible(true);
     }
 
+    //Ranndomiser
     private void moveToRandomCorner() {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int width = getWidth();
